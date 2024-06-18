9910013e827d29054a6e88e02f601a8a96d1e596
JedGStumpf <jedgstumpf@gmail.com>
2024-06-18 11:14:23 -0700

    Updated Instructions in Calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,2 +1,11 @@
 
 class calculator:
+    
+    # Create a function called add to add two numbers and return the result
+    
+    # Create a function called subtract to subtract two numbers and return the result
+    
+    # Create a function called factorial to get the factorial of a number and return the result
+    
+    
+ 
