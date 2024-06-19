78853e7b3fb9228127143bdbb3c8bddf5c259d70
singhari <110957426+singhari@users.noreply.github.com>
2024-06-19 01:58:10 +0000

    Python Intro to machine learning ig?

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,11 +1,8 @@
 
 class calculator:
-    
+    pass
     # Create a function called add to add two numbers and return the result
     
     # Create a function called subtract to subtract two numbers and return the result
     
     # Create a function called factorial to get the factorial of a number and return the result
-    
-    
- 
--- weather_service/weather_service.py
+++ weather_service/weather_service.py
@@ -7,7 +7,15 @@ class WeatherService:
         self.cache_duration = 600  # cache duration in seconds
 
     # write a function to get weather which takes a city as a parameter
-
+    def weatherGet(city):
+    
+        
+        response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=4523628bd365a0c61f43c9ba0c9cd1ff&units=imperial')
     # Fetch new data making an API call
-
+        response.raise_for_status
+        data = response.json
+        weather_data ={
+            'temperature': data['main']['temp'],
+            'humidity': data['main']['humidity']
+        }
     # write a function which Updates a cache
